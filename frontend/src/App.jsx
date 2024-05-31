88dde8817729536abc4d0a771209d3fc5f2eb295
Saachi <saachi.20223219@mnnit.ac.in>
2024-05-31 18:42:10 +0530

    navbar,banner,footer added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './component/Navbar'
+import Banner from './component/Banner'
+import Footer from './component/Footer'
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <Banner/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
